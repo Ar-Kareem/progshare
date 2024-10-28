8fe92944be8979b0e8b8819f7267e28ad1bfb520
arkareem <abdulrahman_3000@hotmail.com>
2024-10-28 15:33:09 +0000

    add init venv to init.sh

--- init.sh
+++ init.sh
@@ -9,3 +9,13 @@ else
     echo "Environment file does not exist. Creating..."
     echo "SECRET_KEY=$(python -c 'import secrets; print(secrets.token_hex())')" > $ENV_FILE
 fi
+
+if [ -d "./venv" ]; then
+    echo "Virtual environment exists. Sourcing..."
+    source ./venv/bin/activate
+else
+    echo "Virtual environment does not exist. Creating and sourcing..."
+    python3 -m venv ./venv
+    source ./venv/bin/activate
+    pip install --upgrade pip
+fi
