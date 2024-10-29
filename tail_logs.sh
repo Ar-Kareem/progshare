23928a024da44ba475497b46df5e39e02ea53be0
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-29 16:47:31 +0300

    Update tail_logs.sh

--- tail_logs.sh
+++ tail_logs.sh
@@ -3,4 +3,8 @@ tail -n ${1-100} -f ./logs/nginx/error.log | sed 's/^/nginx_error: /' &
 tail -n ${1-100} -f ./logs/nginx/cart-err.log | sed 's/^/uwsgi: /' & 
 tail -n ${1-100} -f ./logs/redis/redis-server.log | sed 's/^/redis: /' & 
 tail -n ${1-100} -f ./logs/traefik_logs/traefik_access.json | sed 's/^/traefik_acss: /' & 
-tail -n ${1-100} -f ./logs/traefik_logs/traefik_logs.json | sed 's/^/traefik_logs: /'
+tail -n ${1-100} -f ./logs/traefik_logs/traefik_logs.json | sed 's/^/traefik_logs: /' &
+
+# to be able to ctrl-c (looks worse)
+# tail -n ${1-100} -f ./logs/nginx/access.log ./logs/nginx/error.log ./logs/nginx/cart-err.log ./logs/redis/redis-server.log ./logs/traefik_logs/traefik_access.json  ./logs/traefik_logs/traefik_logs.json
+
