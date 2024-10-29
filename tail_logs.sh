8a7f91c47c900d593e1046a9e88cf1e6fb4166c4
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-29 21:50:21 +0300

    make tail_logs script wait and clean up when ctr-c

--- tail_logs.sh
+++ tail_logs.sh
@@ -5,8 +5,10 @@ tail -n ${1-100} -f ./logs/nginx/flaskapp.log | sed 's/^/flaskapp: /' &
 tail -n ${1-100} -f ./logs/redis/redis-server.log | sed 's/^/redis: /' & 
 tail -n ${1-100} -f ./logs/traefik_logs/traefik_access.json | sed 's/^/traefik_acss: /' & 
 tail -n ${1-100} -f ./logs/traefik_logs/traefik_logs.json | sed 's/^/traefik_logs: /' &
-sleep 0.2  # to wait for all tail to finish printing before returning prompt
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+sleep 3153600000  # 100 year slumber
 
-# to be able to ctrl-c (looks worse)
+
+# shorter (looks worse)
 # tail -n ${1-100} -f ./logs/nginx/access.log ./logs/nginx/error.log ./logs/nginx/cart-err.log ./logs/nginx/flaskapp.log ./logs/redis/redis-server.log ./logs/traefik_logs/traefik_access.json  ./logs/traefik_logs/traefik_logs.json
 
