023eea98390cbb49adb962b381fa8398f99b2528
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-29 15:48:53 +0300

    Update tail_logs.sh to accept line num

--- tail_logs.sh
+++ tail_logs.sh
@@ -1,6 +1,6 @@
-tail -n 100 -f ./logs/nginx/access.log | sed 's/^/nginx_acss: /' &
-tail -n 100 -f ./logs/nginx/error.log | sed 's/^/nginx_error: /' & 
-tail -n 100 -f ./logs/nginx/cart-err.log | sed 's/^/uwsgi: /' & 
-tail -n 100 -f ./logs/redis/redis-server.log | sed 's/^/redis: /' & 
-tail -n 100 -f ./logs/traefik_logs/traefik_access.json | sed 's/^/traefik_acss: /' & 
-tail -n 100 -f ./logs/traefik_logs/traefik_logs.json | sed 's/^/traefik_logs: /'
+tail -n ${1-100} -f ./logs/nginx/access.log | sed 's/^/nginx_acss: /' &
+tail -n ${1-100} -f ./logs/nginx/error.log | sed 's/^/nginx_error: /' & 
+tail -n ${1-100} -f ./logs/nginx/cart-err.log | sed 's/^/uwsgi: /' & 
+tail -n ${1-100} -f ./logs/redis/redis-server.log | sed 's/^/redis: /' & 
+tail -n ${1-100} -f ./logs/traefik_logs/traefik_access.json | sed 's/^/traefik_acss: /' & 
+tail -n ${1-100} -f ./logs/traefik_logs/traefik_logs.json | sed 's/^/traefik_logs: /'
