e726a131495c112265d768810b2c59915fc58044
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-29 06:00:12 +0300

    add echo to scripts

--- db_backups/get_db_backup.ps1
+++ db_backups/get_db_backup.ps1
@@ -1,1 +1,3 @@
+echo "calling: wsl -e ./get_db_backup.sh"
+
 wsl -e ./get_db_backup.sh
--- db_backups/get_db_backup.sh
+++ db_backups/get_db_backup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-
+echo "getting database backup"
 rsync -r --rsync-path="sudo rsync" master@192.168.100.243:/home/master/progshare/redis/dump.rdb ./z_$(date +%Y_%m_%d_%H_%M).rdb
+echo "script done"
